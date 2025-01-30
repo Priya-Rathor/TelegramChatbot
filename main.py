eaa19765193104c741c67317682967a13aeffcde
Priya-Rathor <priya.rathor.266393@gmail.com>
2025-01-30 15:06:40 +0530

    .

--- main.py
+++ main.py
@@ -23,6 +23,20 @@ model_name = "gpt-3.5-turbo"
 bot =Bot(token=TELEGRAM_BOT_TOKEN)
 dispatcher = Dispatcher(bot)
 
+def clear_past():
+    """
+    A function to  clear the previous conversation  and context.
+    """
+
+    reference.response = ""
+
+@dispatcher.message_handler(commands=['clear'])
+async def clear(message:types.Message):
+    """
+    A handler to clear the previouse conversation  and  context.
+    """
+    clear_past()
+    await message.reply("I've cleared the past conversation and  context.")
 
 @dispatcher.message_handler(commands=['start'])
 async def welcome(message:types.Message):
@@ -50,7 +64,24 @@ async def helper(message:types.Message)
     await message.reply(help_command)
 
 
+@dispatcher.message_handler()
+async def chatgpt(message: types.Message):
+    """
+    A handler to process the  user's input and  generate using the  chatGPT API.
+    """
+
+    print(f">>> USER:\n\t{message.text}")
+    response = openai.ChatCompletion.create(
+        model = model_name,
+        messages = [
+            {"role":"assistant",'content':reference.response},
+            {"role":"user","content":message.text}
+        ]
+    )
+    reference.response = response['choices'][0]['message']['content']
+    print(f">>> chatGPT: \n\t{reference.response}")
+    await bot.send_message(chat_id = message.chat.id,  text =reference.response)
 
 
 if __name__ == "__main__":
-  executor.start_polling(dispatcher,skip_updates = True  )
+  executor.start_polling(dispatcher,skip_updates = True)
